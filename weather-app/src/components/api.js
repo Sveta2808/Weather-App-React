124864a30ecf7cefce20dd10f643e6e2be90a6ee
SvetaNazarova <nazarovas84@gmail.com>
2023-05-24 15:26:10 +0300

    weather app added

---
+++ weather-app/src/components/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "651a9f4df6msh7a0d62fd5b84370p1f8947jsn9845d1480373",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "1f41715008ee143ccdff4ac5a8815c92"; 
